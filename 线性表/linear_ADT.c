2e9701a034d82c274981a954ee1959b3fbd14e66
brokensnow2 <2519983675@qq.com>
2023-03-04 08:35:56 +0000

    ...

---
+++ 线性表/linear_ADT.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define InitSize 100
+#define Ture 1
+#define False 0
+typedef struct{
+    int *data;
+    int Max;
+    int length;
+}List;
+
+int *create()
+{
+	return (int*)malloc(sizeof(int)*InitSize);
+}
+
+List* expand(List *l)
+{
+    l->Max = l->Max + 50;
+    return realloc(l->data,sizeof(int)*(l->Max));//caution!!
+}
+
+int Insert(List *list,int i,int e)
+{
+	if(i<1 || i>list->length)
+    	return False;
+    if(list->length>list->Max)
+    {
+        list=expand(list);
+    }
+    for(int j=list->length;j>=i;j--)
+    {
+        list->data[j] = list->data[i];
+    }
+    list->data[i-1] = e;
+    list->length++;
+    return Ture;
+}
+
+int Delete(List* l, int i)
+{
+    for(int j = i;j<l->length;j++)
+    {
+        l->data[j-1]=l->data[j];
+    }
+    l->length--;
+    return Ture;
+}
+
+int main()
+{
+    List* l = (List*)malloc(sizeof(List));
+    l->data = create();
+    for(int i=0;i<10;i++)
+    {
+        l->data[i]=i;
+        l->length++;
+    }
+    for(int i=0;i<10;i++)
+    {
+        printf("%d",l->data[i]);
+    }
+    printf("\nThe Length is %d\n",l->length);
+}
