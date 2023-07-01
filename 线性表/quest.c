75818460dbfc73b613e1105c92117b2928a17c43
brokensnow2 <2519983675@qq.com>
2023-07-01 07:25:20 +0000

    ...

--- 线性表/quest.c
+++ 线性表/quest.c
@@ -2,8 +2,10 @@
 #include<stdlib.h>
 #include"linear_ADT.c"
 
+//QUEST1
 int deleteMinValue(List* list)
 {
+    int min,index;
     if(list -> length == 0)
     {
         printf("error\n");
@@ -11,12 +13,41 @@ int deleteMinValue(List* list)
     }
     else
     {
-		
+        min = list->data[0];
+		for(int i=0;i<list->length;i++)
+        {
+            int a = list->data[i];
+            if(min > a)
+            {
+				min = a;
+                index = i;
+            }
+        }
+        list->data[index] = list->data[list->length-1];
+        return min;
     }
+}
+
+//QUEST2
+void Reverse(List* list)
+{
+    
 }
 
 int main()
 {
-
+    List* l = (List*)malloc(sizeof(List));
+    l->data = create();
+    for(int i=0;i<10;i++)
+    {
+        l->data[i]=i;
+        l->length++;
+    }
+    for(int i=0;i<10;i++)
+    {
+    	printf("%d\t",l->data[i]);        
+    }
+    printf("\n");
+    printf("%d\n",deleteMinValue(l));
 
 }
