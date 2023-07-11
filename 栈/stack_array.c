f4fbfa3997d80ecdbce92a48470bd08e9e51c29a
brokensnow2 <2519983675@qq.com>
2023-07-11 07:31:31 +0000

    ..

--- 栈/stack_array.c
+++ 栈/stack_array.c
@@ -11,6 +11,49 @@ struct stackByarray
 	int top; //数组下标表示栈頂
 };
 
+//用两个栈模拟队列
+//入就放到s1,s1就相当于尾
+//出从s2出，s2就相当于头
+//保持s1总空，s2最新
+void enQueue(stack* s1,stack* s2,int x)
+{
+	//真空
+	if(s2->top == -1)
+	{
+		push(s2,x);	
+	}
+	else
+	{
+		//s2有最新的元素
+		while(s2->top != -1) //while(!StackEmpty(s2))
+		{
+			int temp = pop(s2);
+			push(s1,temp);
+			//x放到队尾
+			push(s1,x);
+		}
+		//全放到s2
+		while(s1->top != -1)
+		{
+			int temp = pop(s1);
+			push(s2,temp);
+		}
+	}
+}
+
+int deQueue(stack* s1,stack* s2)
+{
+	//这是真空了
+	if(s2->top == -1) //while(StackEmpty(s2))
+	{
+		return -9999;
+	}
+	else
+	{
+		return pop(s2);
+	}
+}
+
 int main()
 {
 
--- 队列/queue.c
+++ 队列/queue.c
@@ -61,3 +61,14 @@ int deQueue(Queue *q)
         return x;
     }
 }
+
+int main()
+{
+    
+    short usi = -1;
+    short si = -32768;
+    printf("%x\n",si+usi);
+    //7fff,但双标志？扩展到4字节了？
+    printf("%d\n",si+usi);
+
+}
