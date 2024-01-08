d051cf956160917602f07341169ae840efefdd74
brokensnow2 <2519983675@qq.com>
2024-01-08 17:00:46 +0800

    py

--- PAT/1002.py
+++ PAT/1002.py
@@ -1,11 +1,12 @@
 str1 = input()
 str2 = input()
+# split()方法用于将字符串分割成多个子字符串，并将这些子字符串作为列表返回。如果没有指定分隔符，那么默认的分隔符是任何空白字符，包括空格、换行符(\n)和制表符(\t)等
 list1 = str1.split()
 list2 = str2.split()
 maxDegreeA = list1[0]
 exponentsA = []
 coefficientsA = []
-
+# range(5):生成可迭代的序列[0,1,2,3,4]
 for i in range(int(maxDegreeA)):
     exponentsA.append(int(list1[2*i+1]))
     coefficientsA.append(float(list1[2*i+2]))
